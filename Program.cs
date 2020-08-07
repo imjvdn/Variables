431ab08f949ae14aa035499ee757ad7650e70ebd
Jadan morrow <jadanmorrow@outlook.com>
2020-08-07 07:23:25 -0600

    Adding files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine(characterName + " really liked his name but he didnt like being " + characterAge + ".");
+
+            Console.ReadLine();
+        }
+    }
+}
